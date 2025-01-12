65b84f1d7d7d9f5cab88ae799feec6eb3ffec4ad
= <=>
2025-01-12 11:50:01 +0530

    added

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -67,8 +67,8 @@ const Header = () => {
       {/* Mobile Navigation Menu */}
       {isMobileMenuOpen && (
         <div className="md:hidden">
-          <nav className="flex flex-col space-y-2 p-4">
-            <a href="#" className="text-gray-600 hover:text-blue-600">
+          <nav className="flex flex-col space-y-2 p-4 items-end">
+            {/* <a href="#" className="text-gray-600 hover:text-blue-600">
               Home
             </a>
             <a href="#" className="text-gray-600 hover:text-blue-600">
@@ -85,7 +85,14 @@ const Header = () => {
               className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
             >
               Login
-            </a>
+            </a> */}
+
+            <Link
+              to="/add-account"
+              className="max-w-sm px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
+            >
+              Add Account
+            </Link>
           </nav>
         </div>
       )}
